4a4ead812859f2ed3a381b1842e4725a5c4808dc
zhenghui <15270855286@163.com>
2022-12-11 14:42:14 +0800

    创建数字验证码服务

---
+++ verficationcode/src/test/java/com/ethan/online/taxi/verficationcode/VerficationcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ethan.online.taxi.verficationcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerficationcodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
