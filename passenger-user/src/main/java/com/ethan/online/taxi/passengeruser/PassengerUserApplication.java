642c6c88a2d0bcfc24a39d2a93df5bb1d52fab65
zhenghui <15270855286@163.com>
2022-12-17 15:41:23 +0800

    乘客用户服务搭建

---
+++ passenger-user/src/main/java/com/ethan/online/taxi/passengeruser/PassengerUserApplication.java
@@ -0,0 +1,13 @@
+package com.ethan.online.taxi.passengeruser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassengerUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PassengerUserApplication.class, args);
+    }
+
+}
